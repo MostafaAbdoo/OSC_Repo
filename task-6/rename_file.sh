a91fd91700e5a2a5a7f5d94613b98cfe649d0d5e
Mostafa Abdo <160092209+MostafaAbdoo@users.noreply.github.com>
2024-08-27 18:56:55 +0300

    Add files via upload

---
+++ task-6/rename_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+directory="$1"
+
+if [[ ! -d "$directory" ]]; then
+  echo "Directory $directory does not exist."
+  exit 1
+fi
+
+for file in "$directory"/*.txt; do
+  if [[ -f "$file" ]]; then
+    filename=$(basename "$file")
+    mv "$file" "$directory/old_$filename"
+  fi
+done
+
+echo "done"
+
+(my laptop is broken i don't have any option but to send it using GUI)
